d36b9aabfe054e36097c9822e4a9051e244675e3
venugopalgithub1991 <156532909+venugopalgithub1991@users.noreply.github.com>
2024-03-19 21:19:27 +0530

    Update deploy.sh

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker login -u venugopal1991 -p venugopal@1991
+docker login -u venugopal1991 -p Venugopal@1991
 docker push venugopal1991/repo_dev:dev
 docker push venugopal1991/repo_prod:prod
